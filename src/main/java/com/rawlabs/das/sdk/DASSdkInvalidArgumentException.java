584f6777f8cb05ee615011d5b3ee0bf220f30fc2
Benjamin Gaidioz <ben@raw-labs.com>
2025-02-26 16:41:35 +0100

    fmt

--- src/main/java/com/rawlabs/das/sdk/DASSdkInvalidArgumentException.java
+++ src/main/java/com/rawlabs/das/sdk/DASSdkInvalidArgumentException.java
@@ -13,15 +13,15 @@
 package com.rawlabs.das.sdk;
 
 /**
- * DASSdkInvalidArgumentException is thrown by DAS SDK methods when an invalid argument error is to be reported
- * to a user (e.g. string too long during an INSERT, missing mandatory predicate, etc.).
+ * DASSdkInvalidArgumentException is thrown by DAS SDK methods when an invalid argument error is to
+ * be reported to a user (e.g. string too long during an INSERT, missing mandatory predicate, etc.).
  */
 public class DASSdkInvalidArgumentException extends RuntimeException {
-    public DASSdkInvalidArgumentException(String message) {
-        super(message);
-    }
+  public DASSdkInvalidArgumentException(String message) {
+    super(message);
+  }
 
-    public DASSdkInvalidArgumentException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public DASSdkInvalidArgumentException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
